5ed8fa2d420ca7079e53d04da5a1467ae4c57079
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-10 00:01:32 +0000

    endpoint skeletons for update to skills, blog posts, projects, and their respective categories

--- main.py
+++ main.py
@@ -28,18 +28,28 @@ class Skill_Category(BaseModel):
 def read_item(id_site: int):
     return data_skills
 
+#Update a Skill
+@app.post("/update_skill/{id_site}")
+def read_item(id_site: int, id_skill: int):
+    return {}
+
 #Add a Skill
 # api/items/[x]?skill=[y]
 @app.put("/add_skills_item/{id_site}")
 def read_item(id_site: int, skill: Skill):
     return {}
 
-#Fetch the Skill' Categories
+#Fetch Categories of Skills
 @app.get("/get_skills_categories/{id_site}")
 def read_item(id_site: int):
     return {}
 
-#Add a Category for the Skills
+#Update a Category of Skills
+@app.post("/update_skills_category/{id_site}")
+def read_item(id_site: int, id_skills_category: int):
+    return {}
+
+#Add a Category of Skills
 @app.put("/add_skills_category/{id_site}")
 def read_item(id_site: int, skills_category: Skill_Category):
     return {}
@@ -53,17 +63,27 @@ def read_item(id_site: int, skills_category: Skill_Category):
 def read_item(id_site: int):
     return data_projects
 
+#Update a Project
+@app.post("/update_project/{id_site}")
+def read_item(id_site: int, id_project: int):
+    return {}
+
 #Add a Project
 @app.put("/add_project/{id_site}")
 def read_item(id_site: int, project: Union[str, None] = None):
     return {}
 
-#Fetch the Projects' Categories
+#Fetch the Categories of Projects
 @app.get("/get_projects_categories/{id_site}")
 def read_item(id_site: int):
     return {}
 
-#Add a Category for the Projects
+#Update a Category of Projects
+@app.post("/update_projects_category/{id_site}")
+def read_item(id_site: int, id_projects_category: int):
+    return {}
+
+#Add a Category of Projects
 @app.put("/add_projects_category/{id_site}")
 def read_item(id_site: int, projects_category: Union[str, None] = None):
     return {}
@@ -77,17 +97,27 @@ def read_item(id_site: int, projects_category: Union[str, None] = None):
 def read_item(id_site: int):
     return {}
 
+#Update a Blog Post
+@app.post("/update_blog_post/{id_site}")
+def read_item(id_site: int, id_blog_post: int):
+    return {}
+
 #Add a Blog Post
 @app.put("/add_blog_post/{id_site}")
 def read_item(id_site: int, blog_post: Union[str, None] = None):
     return {}
 
-#Fetch the Blog Posts' Categories
+#Fetch the Categories of Blog Posts
 @app.get("/get_blog_categories/{id_site}")
 def read_item(id_site: int):
     return {}
 
-#Add a Category for the Blog Posts
+#Update a Category of the Blog Posts
+@app.post("/update_blog_category/{id_site}")
+def read_item(id_site: int, id_blog_category: int):
+    return {}
+
+#Add a Category of the Blog Posts
 @app.put("/add_blog_category/{id_site}")
 def read_item(id_site: int, blog_category: Union[str, None] = None):
     return {}
