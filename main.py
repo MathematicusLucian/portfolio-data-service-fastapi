360bf7cc60cf32e8d133d62d7476fad96a42ac10
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-16 04:29:22 +0000

    tidied endpoints

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ app = FastAPI()
 @app.get(
     "/get_menu_main/{id_site}"
 )
-async def get_main_menu(id_site: int) -> list[Menu_Data]:
+async def get_main_menu(id_site: int): # -> list[Menu_Data]:
     if data_main_menu:
         return data_main_menu
     return JSONResponse(status_code=404, content={"message": "Item not found"})
