c073fe1cd18e19b39896d0bc8f53bcb30dfe3851
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-22 04:44:02 +0000

    tweaked of_categoryfor menu_items

--- app/routers/menu_routers.py
+++ app/routers/menu_routers.py
@@ -10,7 +10,7 @@ from app.models.blog_models import Blog_Post_Category_Data, Blog_Post_Data
 from app.services.database_manager import DatabaseManagerInterface, get_database
 
 menu_router = APIRouter(prefix='/menu_items')
-collection_name = 'skills'
+collection_name = 'menu_items'
 
 #--------#
 # Menus #
@@ -20,18 +20,18 @@ collection_name = 'skills'
 
 # # READ
 @menu_router.get("/all")
-async def all(database_manager_service: DatabaseManagerInterface = Depends(get_database)): #list[Blog_Post_Category_Data]:
-    menu = await database_manager_service.all('menu_items')
+async def all(database_manager_service: DatabaseManagerInterface = Depends(get_database)):
+    menu = await database_manager_service.all(collection_name)
     return menu
 
 @menu_router.get("/of_category/{id_category}")
-async def all_skills_of_category(id_category: str, database_manager_service: DatabaseManagerInterface = Depends(get_database)): #list[Blog_Post_Category_Data]:
-    menu = await database_manager_service.all('menu_items', id_category)
+async def all_skills_of_category(id_category: str, database_manager_service: DatabaseManagerInterface = Depends(get_database)):
+    menu = await database_manager_service.all(collection_name, id_category)
     return menu
 
 @menu_router.get('/read/{id_menu_item}')
 async def one(id_menu_item: str, database_manager_service: DatabaseManagerInterface = Depends(get_database)):
-    menu_item = await database_manager_service.one_item(id_menu_item, 'menu_items')
+    menu_item = await database_manager_service.one_item(id_menu_item, collection_name)
     return menu_item
 
 # # UPDATE
